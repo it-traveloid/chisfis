c95005ebf50a6845b8a3745d442f7849aaf6c57c
it-traveloid <81027532+it-traveloid@users.noreply.github.com>
2023-07-06 00:32:07 +0800

    Update 4 AnyReactComponent.tsx

--- components/travel/components/AnyReactComponent/AnyReactComponent.tsx
+++ components/travel/components/AnyReactComponent/AnyReactComponent.tsx
@@ -1,6 +1,6 @@
 import { Transition } from "@headlessui/react";
 import CarCard from "../../components/CarCard/CarCard"; 
-import ExperiencesCard from "components/ExperiencesCard/ExperiencesCard";
+import ExperiencesCard from "../../components/ExperiencesCard/ExperiencesCard";
 import StayCard from "../../components/StayCard/StayCard";
 import { CarDataType, ExperiencesDataType, StayDataType } from "data/types";
 import React, { FC, Fragment } from "react";
